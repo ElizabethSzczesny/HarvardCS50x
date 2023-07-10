d4cfd7815457d2e695af0f8c08a849c63c0ce38d
Elizabeth Szczesny <elizabeth.szczesny@gmail.com>
2023-07-10 15:54:45 -0400

    Final Project

---
+++ Final Project/script.js
@@ -0,0 +1,66 @@
+var target = document.querySelectorAll('.target');
+var move = document.querySelectorAll('.move');
+var append = document.querySelectorAll('.append');
+
+console.log("Hello World");
+
+var counter = 0;
+
+move[0].addEventListener("mouseover", function(){
+    if(counter == 0){
+        var clone = move[0].cloneNode(true);
+        append[0].appendChild(clone);
+        counter += 1
+    }
+    var node = document.createTextNode('move to match the word pine cone with the image of a pine cone');
+    append[0].appendChild(node);
+    }
+)
+
+//Test
+//Make the DIV element draggagle:
+dragElement(document.getElementById("mydiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+//target.addEventListener()
+
