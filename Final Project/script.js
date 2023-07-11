0f262cd99a8b7a60e65d869d810bea568de4ea6b
Elizabeth Szczesny <elizabeth.szczesny@gmail.com>
2023-07-11 17:44:01 -0400

    Final Project

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,4 +1,4 @@
-var target = document.querySelectorAll('.target');
+/*var target = document.querySelectorAll('.target');
 var move = document.querySelectorAll('.move');
 var append = document.querySelectorAll('.append');
 
@@ -15,52 +15,56 @@ move[0].addEventListener("mouseover", function(){
     var node = document.createTextNode('move to match the word pine cone with the image of a pine cone');
     append[0].appendChild(node);
     }
-)
+)*/
 
-//Test
 //Make the DIV element draggagle:
-dragElement(document.getElementById("mydiv"));
+const drag = document.getElementById("dragtarget");
+const drop = document.getElementById("droptarget");
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+/* Events fired on the drag target */
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+drag.addEventListener("dragstart", function(event) {
+  event.dataTransfer.setData("Text", event.target.id);
+});
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+drag.addEventListener("drag", function(event) {
+  //document.getElementById("demo").innerHTML = "The text is being dragged.";
+  drop.style.visibility = "visible";
+});
 
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
+
+/* Events fired on the drop target */
+drop.addEventListener("dragover", function(event) {
+  event.preventDefault();
+  //document.getElementById("demo").innerHTML = "The text is OVER the droptarget.";
+  drop.style.visibility = "visible";
+  //event.target.style.border = "4px dotted purple";
+});
+
+var pagenumber = 1;
+
+drop.addEventListener("drop", function(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+  //document.getElementById("demo").innerHTML = "The text was dropped.";
+  drop.style.visibility = "visible";
+  setTimeout(turnPage, 1800);
+  pagenumber +=1
+  if (pagenumber == 5){
+    pagenumber = null;
   }
+});
+
+function turnPage(){
+  window.location = "index" + pagenumber + ".html";
 }
 
-//target.addEventListener()
+
+
+
+
+
+
+
 
