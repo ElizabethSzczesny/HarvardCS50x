d03f1b0cbaceb9143804095d62e99d0371008692
Elizabeth Szczesny <elizabeth.szczesny@gmail.com>
2023-07-13 22:50:01 -0400

    Final Project

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,14 +1,17 @@
-/*var target = document.querySelectorAll('.target');
-var move = document.querySelectorAll('.move');
+const drag = document.getElementById("dragtarget");
+const drop = document.getElementById("droptarget");
+
 var append = document.querySelectorAll('.append');
 
 console.log("Hello World");
 
 var counter = 0;
 
-move[0].addEventListener("mouseover", function(){
+//put all your id's into their own array manually and then looped through that
+
+/*drag.addEventListener("mouseover", function(){
     if(counter == 0){
-        var clone = move[0].cloneNode(true);
+        var clone = drag.cloneNode(true);
         append[0].appendChild(clone);
         counter += 1
     }
@@ -18,8 +21,7 @@ move[0].addEventListener("mouseover", function(){
 )*/
 
 //Make the DIV element draggagle:
-const drag = document.getElementById("dragtarget");
-const drop = document.getElementById("droptarget");
+
 
 /* Events fired on the drag target */
 
@@ -38,7 +40,7 @@ drop.addEventListener("dragover", function(event) {
   event.preventDefault();
   //document.getElementById("demo").innerHTML = "The text is OVER the droptarget.";
   drop.style.visibility = "visible";
-  //event.target.style.border = "4px dotted purple";
+  //event.target.style.border = "4px dotted brown";
 });
 
 var pagenumber = 1;
@@ -46,14 +48,22 @@ var pagenumber = 1;
 drop.addEventListener("drop", function(event) {
   event.preventDefault();
   const data = event.dataTransfer.getData("Text");
-  event.target.appendChild(document.getElementById(data));
+  drop.appendChild(document.getElementById(data));
   //document.getElementById("demo").innerHTML = "The text was dropped.";
   drop.style.visibility = "visible";
-  setTimeout(turnPage, 1800);
+
+  if(counter == 0){
+    var clone = drag.cloneNode(true);
+    append[0].appendChild(clone);
+    console.log(clone);
+    counter += 1
+  } 
+
+  /*setTimeout(turnPage, 5000);
   pagenumber +=1
   if (pagenumber == 5){
     pagenumber = null;
-  }
+  }*/
 });
 
 function turnPage(){
